4222e742496584c95bde640aa890e6d6f2fb5de7
Your Name <you@example.com>
2022-09-02 21:55:13 +0400

    modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,88 +1,43 @@
-class GoodsItem {
-    constructor(title, price, img) {
-        this.title = title;
-        this.price = price;
-        this.img = img
-    }
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-    render() {
-        return `<div class="goods-item">
-                <img class='image-item src="${img}" alt="фото"'/>
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
-                <button class="button-item">Купить</button>
-            </div >`
-    }
-}
-
-class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-
-    fetchGoods() {
-        makeGETRequest(`${API_URL}/catalogData.json`)
-            .then((goods) => {
-                this.goods = JSON.parse(goods);
-                console.log(`${goods}`)
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        filtered: [],
+        imgCatalog: 'https://via.placeholder.com/200x150',
+        userSearch: '',
+        show: false
+    },
+    methods: {
+        filter(){
+         const regexp = new RegExp(this.userSearch, 'i');
+         this.filtered = this.products.filter(product => regexp.test(product.product_name));
+        },
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product){
+                console.log(product.id_product);
+        }
+    },
+    mounted(){
+       this.getJson(`${API + this.catalogUrl}`)
+           .then(data => {
+               for(let el of data){
+                   this.products.push(el);
+               }
+           });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                }
             })
-            .then(() => { this.render() })
-            .catch(err => { console.error('err') })
     }
-
-    getSum() {
-
-        let sumGoods = 0;
-        this.goods.forEach(good => {
-            sumGoods += good.price;
-            // console.log(`${sumGoods}`);
-        })
-        console.log(`Общая сумма состовляет ${sumGoods} рублей`)
-    }
-}
-
-class Basket {
-    constructor() {
-
-    }
-
-    addGood() {
-
-    }
-
-    updateGood() {
-
-    }
-
-    removeGood() {
-
-    }
-
-    render() {
-
-    }
-}
-
-class ElemBasket {
-    render() {
-
-    }
-}
-
-
-// const list = new GoodsList();
-// list.fetchGoods();
-// list.getSum();
-
-function makeGETRequest(url) {
-
-    return new Promise((resolve, reject) => {
-        let time = Math.random() * 100;
-        console.log(time);
-
-        b.send();
-        b.open('GET', url, true);
-
-        return Promise;
-    });
-};
+})
